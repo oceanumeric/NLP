ced25afd625bbaeef6f7140c9d5d86cae4d01128
oceanumeric <numerical.ocean@gmail.com>
2022-05-12 17:28:19 +0000

    [update] chow.py

--- word_embeddings/cbow.py
+++ word_embeddings/cbow.py
@@ -1,12 +1,13 @@
 # given a list of words [sliced with a fixed window size]
 # predict the center word 
 import os
-from struct import pack
-from scipy.misc import logsumexp
 import nltk
 import re
-from nltk.tokenize import word_tokenize
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import logsumexp
+from scipy import linalg
+from nltk.tokenize import word_tokenize 
 from collections import Counter, defaultdict
 
 
@@ -76,6 +77,7 @@ class CBOW:
             x = np.zeros(v)
             center_word = self.token[i]
             y[self.word2ind[center_word]] = 1 
+            # context window size = 4 (without counting center word)
             context_words = self.token[(i-center):i] + self.token[(i+1):(i+center+1)]
             num_ctx_words = len(context_words)
             for idx, freq in self._pack_idx_with_frequency(context_words, self.word2ind):
@@ -105,10 +107,9 @@ class CBOW:
                 batch_y.append(y)
             else:
                 yield np.array(batch_x).T, np.array(batch_y).T
-                batch = []
        
     
-    def _initialize_model(self, N, random_seed=177):
+    def _initialize_model(self, N, random_seed=1):
         '''
         Input:
             N - dimension of hidden vector
@@ -135,7 +136,7 @@ class CBOW:
         z = np.float128(z)
         z_exp = np.exp(z)
         y_hat = z_exp / np.sum(z_exp, axis=0) 
-        return y_hat
+        return y_hat.T
     
     def _forward_prop(self, x, w1, b1, w2, b2):
         '''
@@ -149,22 +150,26 @@ class CBOW:
         h = np.maximum(0, h)
         z = w2 @ h + b2  # w2 shape = (v, n)
         
-        return z, h
+        return z.T, h.T
     
-    def _compute_cost(self, y, yhat, batch_size):
+    def _compute_cost(self, z, y, yhat, c, batch_size):
+        '''
+        Input:
+            z: shape = (v, n)
+        '''
         # element-wise multiplication 
-        log_prob = np.multiply(np.log(yhat), y) + np.multiply(np.log(1-yhat), 1-y)
-        # maximum likelihood 
-        cost = -1/batch_size * np.sum(log_prob)
+        z_hat = logsumexp(z, axis=1, keepdims=True)
+        cost = (-np.sum(y*np.log(yhat)) + np.sum(2.0*c*z_hat)) / batch_size
         cost = np.squeeze(cost)  # remove dimension (shape) indices 
         
         return cost
         
-    def _back_prop(self, x, yhat, y, h, w1, b1, w2, b2, batch_size):
+    def _back_prop(self, x, yhat, y, h, w1, b1, w2, b2, batch_size, m):
         '''
         Input:
             as it states
             batch_size: the training batch 
+            m: number of context words
         Output:
             gradients 
             grad_w1, grad_w2, grad_b1, grad_b2
@@ -172,14 +177,14 @@ class CBOW:
         w2y = np.dot(w2.T, (yhat - y)) 
         
         w2yrelu = np.maximum(0, w2y)
-        grad_w1 = (1/batch_size)*np.dot(w2yrelu, x.T)  # coefficient of w1 = X 
-        grad_b1 = (1/batch_size)*np.sum(np.dot())
-        grad_w2 = np.dot(yhat-y, h.T)
-        grad_b2 = yhat - y
+        grad_w1 = (1/(batch_size*m))*np.dot(w2yrelu, x.T)  # coefficient of w1 = X 
+        grad_b1 = (1/batch_size)*np.sum(w2yrelu, axis=1, keepdims=True)
+        grad_w2 = (1/(batch_size*m))*np.dot(yhat-y, h)
+        grad_b2 = (1/(batch_size*m))*np.sum((yhat-y), axis=1, keepdims=True)
         
         return grad_w1, grad_b1, grad_w2, grad_b2
     
-    def gradient_descent(self, N, num_iters, alpha=0.3):
+    def gradient_descent(self, N, num_iters, alpha=0.03):
         '''
         Input:
             text data: self.token, self.word2ind
@@ -190,21 +195,21 @@ class CBOW:
             updated gradients: w1, b1, w2 b2
         '''
         w1, b1, w2, b2 = self._initialize_model(N)
+        center = 2
+        m = 2*center  # context window [i am happy(center) become I]
         batch_size = 128
         iters = 0
-        center = 2
-        
         for x, y in self._get_batches(center, batch_size):
             z, h = self._forward_prop(x, w1, b1, w2, b2)
             yhat = self._softmax(z)
-            cost = self._compute_cost(y, yhat, batch_size)
+            cost = self._compute_cost(z, y, yhat, center, batch_size)
             
-            if ((iters + 1) % 10 == 0):
+            if ((iters + 1) % 2 == 0):
                 print(f"iterations: {iters + 1} cost: {cost:.6f}")
             # get gradients 
             grad_w1, grad_b1, grad_w2, grad_b2 = self._back_prop(x, yhat, y, h,
                                                                  w1, b1, w2, b2,
-                                                                 batch_size)
+                                                                 batch_size, m)
             w1 = w1 - alpha * grad_w1
             b1 = b1 - alpha * grad_b1
             w2 = w2 - alpha * grad_w2
@@ -218,19 +223,58 @@ class CBOW:
                 alpha *= 0.66
                 
         return w1, b1, w2, b2
-        
-        
-        
-        
     
-    
+
+def compute_pca(data, n_components=2):
+    """
+    Input: 
+        data: of dimension (m,n) where each row corresponds to a word vector
+        n_components: Number of components you want to keep.
+    Output: 
+        X_reduced: data transformed in 2 dims/columns + regenerated original data
+    pass in: data as 2D NumPy array
+    """
+
+    m, n = data.shape
+
+    ### START CODE HERE ###
+    # mean center the data
+    data -= data.mean(axis=0)
+    # calculate the covariance matrix
+    R = np.cov(data, rowvar=False)
+    # calculate eigenvectors & eigenvalues of the covariance matrix
+    # use 'eigh' rather than 'eig' since R is symmetric,
+    # the performance gain is substantial
+    evals, evecs = linalg.eigh(R)
+    # sort eigenvalue in decreasing order
+    # this returns the corresponding indices of evals and evecs
+    idx = np.argsort(evals)[::-1]
+
+    evecs = evecs[:, idx]
+    # sort eigenvectors according to same index
+    evals = evals[idx]
+    # select the first n eigenvectors (n is desired dimension
+    # of rescaled data array, or dims_rescaled_data)
+    evecs = evecs[:, :n_components]
+    ### END CODE HERE ###
+    return np.dot(evecs.T, data.T).T
+
     
 if __name__ == "__main__":
     print(os.getcwd())
     print(defaultdict(int))
     word2vec = CBOW('./word_embeddings/shakespeare.txt')
     # train the model N = 50, iteration = 150
-    w1, b1, w2, b2 = word2vec.gradient_descent(50, 150)
+    w1, b1, w2, b2 = word2vec.gradient_descent(50, 15)
+    print(w1.shape, b1.shape, w2.shape, b2.shape)
+    words = ['king', 'queen','lord','man', 'woman','dog','horse',
+         'rich','happy','sad']
+    embs = (w1.T+w2)/2
+    idx = [word2vec.word2ind[word] for word in words]
+    words_vector = embs[idx, :]
+    print(words_vector.shape)
+    
+     
     
     
 
