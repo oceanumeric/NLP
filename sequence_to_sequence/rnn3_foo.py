589a0ac3a649e135b1026ee89072f5540c4eb392
oceanumeric <numerical.ocean@gmail.com>
2023-06-10 23:27:00 +0200

    [update] rnn3

--- sequence_to_sequence/rnn3_foo.py
+++ sequence_to_sequence/rnn3_foo.py
@@ -209,6 +209,85 @@ class SimpleRNN(nn.Module):
             xyield = xdata[:, i : i + seq_length]
             yyield = ydata[:, i : i + seq_length]
             yield xyield, yyield
+
+
+    # function to predict the next character based on character
+    def predict(self, char, h=None, top_k=None):
+        # assume char is a single character
+        # convert the character to integer
+        char = torch.tensor([[self.char_to_int[char]]])
+        # push the character to the GPU
+        char = char.to(device)
+        # one-hot encode the character
+        inputs = F.one_hot(char, self.vocab_size).float()
+
+        # initialize the hidden state
+        if h is None:
+            # h.shape = (1, hidden_size)
+            # because we only have one character
+            h = torch.zeros((1, self.hidden_size))
+
+        # push the hidden state to the GPU
+        h = h.to(device)
+
+        # call the model to get the output and hidden state
+        with torch.no_grad():
+            # get the output and hidden state
+            output, h = self(inputs, h)
+            # output.shape = (1, vocab_size)
+
+        # get the probabilities
+        # dim=1 because we want to get the probabilities for each character
+        p = F.softmax(output, dim=1).data
+
+        # if top_k is None, we will use torch.multinomial to sample
+        # otherwise, we will use torch.topk to get the top k characters
+        if top_k is None:
+            # reshape p as (vocab_size)
+            p = p.reshape(self.vocab_size)
+            # sample with torch.multinomial
+            char_next_idx = torch.multinomial(p, num_samples=1)
+            # char_next_idx.shape = (1, 1)
+            # convert the index to character
+            char_next = self.int_to_char.get(char_next_idx.item())
+        else:
+            # since we have many characters,
+            # it is better to use torch.topk to get the top k characters
+            p, char_next_idx = p.topk(top_k)
+            # char_next_idx.shape = (1, top_k)
+            # convert the index to character
+            char_next_idx = char_next_idx.squeeze().cpu().numpy()
+            # char_next_idx.shape = (top_k)
+            # randomly select one character from the top k characters
+            p = p.squeeze().cpu().numpy()
+            # p.shape = (top_k)
+            char_next_idx = np.random.choice(char_next_idx, p=p / p.sum())
+            # char_next_idx.shape = (1)
+            # convert the index to character
+            char_next = self.int_to_char.get(char_next_idx.item())
+
+        return char_next, h
+
+    # function to generate text
+    def generate_text(self, char="a", h=None, length=100, top_k=None):
+        # intialize the hidden state
+        if h is None:
+            h = torch.zeros((1, self.hidden_size))
+        # push the hidden state to the GPU
+        h = h.to(device)
+
+        # initialize the generated text
+        gen_text = char
+
+        # predict the next character until we get the desired length
+        # we are not feedding the whole sequence to the model
+        # but we are feeding the output of the previous character to the model
+        # because the the memory was saved in the hidden state
+        for i in range(length):
+            char, h = self.predict(char, h, top_k)
+            gen_text += char
+
+        return gen_text
     
 
 def predict(model, char, device, h=None, top_k=5):
@@ -279,7 +358,7 @@ if __name__ == "__main__":
     seq_length = 100
     batch_size = 128   # 512
     hidden_size = 512  # or 256
-    epochs = 300
+    epochs = 600
     learning_rate = 0.001
 
     rnn_model = SimpleRNN(data_path="data/sonnets.txt", batch_size=batch_size,
@@ -298,6 +377,8 @@ if __name__ == "__main__":
     # train the model
     loss_list = rnn_model.train_model(epochs=epochs, lr=learning_rate)
 
-     # print(model.generate_text(char="A", length=100, top_k=5))
-    print(sample(rnn_model, 1000, device, prime='A', top_k=5))
+    # print(model.generate_text(char="A", length=100, top_k=5))
+    # print(sample(rnn_model, 1000, device, prime='A', top_k=5))
+    # print(rnn_model.generate_text(char="H", length=100))
+    print(rnn_model.generate_text(char="H", length=500, top_k=5))
 # %%
